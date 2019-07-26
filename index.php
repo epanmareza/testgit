e9d3256eb8707f58305ac3d17ccbea296f13b7ea
epanmareza <16.9104@stis.ac.id>
2019-07-26 09:02:56 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Test Git</title>
 </head>
+
 <body>
     <h3>Hello World !</h3>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque distinctio quod autem a architecto veritatis, dignissimos quas at tempore asperiores, dolorem nam similique alias eius minus illo nulla. Laborum, </p>
+    <ul>
+        <li>Bayani Sy</li>
+        <li>Aprida Marlina</li>
+        <li>Epan Mareza</li>
+        <li>Ilham Agustarino</li>
+        <li>Vinanda Icha Eprilia</li>
+    </ul>
 </body>
+
 </html>
